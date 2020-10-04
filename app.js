2f2d2e0f1824590ae103767627b27dd56264e512
caivan <caivan2@gmail.com>
2020-10-04 13:28:20 -0500

    add helmet

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const router = require('./router');
+const helmet = require('helmet');
+
 require('./database/db.connection');
 
 
 //middlewears
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
