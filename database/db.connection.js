0a429e6de6ee366d46f370a8e54bf186bf0ab39f
caivan <caivan2@gmail.com>
2020-10-16 19:50:47 -0500

    print host

--- database/db.connection.js
+++ database/db.connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const dbconfig = require('./db.config');
-//no se logro conectar la configuracion de la base de datos con el uri de la base de datos
 const devUrl = `mongodb+srv://${dbconfig.USER}:${dbconfig.PASSWORD}@${dbconfig.HOST}/${dbconfig.DB}`;
 const prodUrl = `${process.env.MONGODB_DRIVER_STRING}://${process.env.MONGODB_USR}:${process.env.MONGODB_PWD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DBNAME}`;
 const env = process.env.APP_ENVIRONMENT;
@@ -13,7 +12,7 @@ const db = mongoose.connection;
 //CONFIRMATION OF THE CONNECTION WITH THE DATABASE
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('WE ARE CONNECTED TO', uri);
+  console.log('WE ARE CONNECTED TO', `${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DBNAME}`);
 });
 
 mongoose.connect(uri,{
